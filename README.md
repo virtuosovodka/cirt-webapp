91267427b8eb97ee8b976e33ad1b41d914df0ba8
virtuosovodka <67210448+virtuosovodka@users.noreply.github.com>
2026-08-08 21:53:30 -0700

    credited ai usage as required

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cirt-webapp
 Cyber Incident Response Tracker
+
+AI Tools were used for brainstorming organization, debugging server-side code, file structure organization, and generating CSS styles. I used Claude Sonnet 4.6 and Google Gemini Flash-Lite as the main models. 
--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
+// This function was debugged using AI to resolve asynchronous route handling issues.
+// The specific use case was to get the user experience of the arrows to toggle the intersection tables, that was implemented with Claude.
+
 function toggleIncidentDetails(row) {
     // Get the details row (next row in the table)
     const detailsRow = row.nextElementSibling;
